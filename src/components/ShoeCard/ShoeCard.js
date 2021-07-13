821564738379460116ec63741a4ecb6bff429408
Étienne Lévesque <etienne.levesque16@gmail.com>
2021-07-12 22:26:56 -0400

    shoe grid B

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
 
 import { COLORS, WEIGHTS } from '../../constants';
 import { formatPrice, pluralize, isNewShoe } from '../../utils';
@@ -35,27 +35,38 @@ const ShoeCard = ({
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
+          {variant === 'new-release' && <Label variant={variant}>Just Released!</Label>}
+          {variant === 'on-sale' && <Label variant={variant}>On Sale!</Label>}
           <Image alt="" src={imageSrc} />
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price discounted={Boolean(salePrice)}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {salePrice && <SalePrice>{formatPrice(salePrice)}</SalePrice>}
         </Row>
       </Wrapper>
     </Link>
   );
 };
 
+const Label = styled.div`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  background-color: ${({ variant }) => variant === 'new-release' ? 'hsl(240, 60%, 63%)' : 'hsl(340, 66%, 47%)'};
+  padding: 8px 10px;
+  color: white;
+  font-weight: bold;
+`
+
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
   flex: 1 1 300px;
-  /* min-width: 260px;
-  max-width: 340px; */
 `;
 
 const Wrapper = styled.article``;
@@ -67,10 +78,13 @@ const ImageWrapper = styled.div`
 
 const Image = styled.img`
   width: 100%;
+  border-radius: 16px 16px 4px 4px;
 `;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -78,7 +92,16 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  ${({ discounted }) => {
+    if (discounted) {
+      return css`
+        text-decoration: line-through;
+        color: hsl(210, 6%, 40%);
+      `
+    }
+  }}
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
